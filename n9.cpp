dd0af8852d0120bca70532807237d542d2d61c53
HotdogWatery <94791310+HotdogWatery@users.noreply.github.com>
2025-07-17 17:07:40 +0300

    Update n9.cpp

--- n9.cpp
+++ n9.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <iomanip>
+#include <limits>
 
 using namespace std;
 
-double calculateAverage(const vector<int>& arr) {
+double calculateAverage(const vector<double>& arr) {
     double sum = 0.0;
-    for (int val : arr) {
-        sum += val;
+    for (double num : arr) {
+        sum += num;
     }
     return sum / arr.size();
 }
 
-int findClosestRow(const vector<vector<int>>& matrix, const vector<double>& columnAverages) {
+int findClosestRow(const vector<vector<double>>& matrix, const vector<double>& columnAverages) {
     int closestRow = 0;
-    double minDiff = numeric_limits<double>::max();
+    double minDifference = numeric_limits<double>::max();
 
-    for (size_t i = 0; i < matrix.size(); ++i) {
-        double totalDiff = 0.0;
-        for (size_t j = 0; j < matrix[i].size(); ++j) {
-            totalDiff += abs(matrix[i][j] - columnAverages[j]);
+    for (int i = 0; i < matrix.size(); ++i) {
+        double rowDifference = 0.0;
+        for (int j = 0; j < matrix[i].size(); ++j) {
+            rowDifference += abs(matrix[i][j] - columnAverages[j]);
         }
 
-        if (totalDiff < minDiff) {
-            minDiff = totalDiff;
+        if (rowDifference < minDifference) {
+            minDifference = rowDifference;
             closestRow = i;
         }
     }
@@ -32,23 +32,13 @@ int findClosestRow(const vector<vector<int>>& matrix, const vector<double>& colu
     return closestRow;
 }
 
-void printMatrix(const vector<vector<double>>& matrix) {
-    for (const auto& row : matrix) {
-        for (double val : row) {
-            cout << setw(8) << fixed << setprecision(2) << val;
-        }
-        cout << endl;
-    }
-}
-
 int main() {
     int n, m;
-    
-    cout << "Введите размеры матрицы (n m): ";
+    cout << "Введите количество строк (n) и столбцов (m) матрицы: ";
     cin >> n >> m;
-    
-    vector<vector<int>> matrix(n, vector<int>(m));
-    
+
+    vector<vector<double>> matrix(n + 1, vector<double>(m));
+
     cout << "Введите элементы матрицы:" << endl;
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
@@ -58,32 +48,24 @@ int main() {
 
     vector<double> columnAverages(m);
     for (int j = 0; j < m; ++j) {
-        vector<int> column;
+        vector<double> column(n);
         for (int i = 0; i < n; ++i) {
-            column.push_back(matrix[i][j]);
+            column[i] = matrix[i][j];
         }
         columnAverages[j] = calculateAverage(column);
+        matrix[n][j] = columnAverages[j];
     }
 
-    vector<vector<double>> extendedMatrix(matrix.begin(), matrix.end());
-    extendedMatrix.push_back(columnAverages);
+    cout << "\nМатрица с добавленной строкой средних значений:" << endl;
+    for (int i = 0; i <= n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
 
     int closestRow = findClosestRow(matrix, columnAverages);
-
-    cout << "\nИсходная матрица с добавленной строкой средних:" << endl;
-    printMatrix(extendedMatrix);
-
-    cout << "\nСредние значения столбцов:" << endl;
-    for (size_t j = 0; j < columnAverages.size(); ++j) {
-        cout << "Столбец " << j + 1 << ": " << fixed << setprecision(2) << columnAverages[j] << endl;
-    }
-
-    cout << "\nСтрока с наиболее близкими к среднему значениями: " << closestRow + 1 << endl;
-    cout << "Элементы этой строки:" << endl;
-    for (int val : matrix[closestRow]) {
-        cout << setw(5) << val;
-    }
-    cout << endl;
+    cout << "\nСтрока, наиболее близкая к средним значениям: " << closestRow + 1 << endl;
 
     return 0;
 }
